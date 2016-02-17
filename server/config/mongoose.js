61f8f2a29b9de5d9452c06d96e341513672dfa8b
chou.thomas <chou.thomas3@gmail.com>
2016-02-16 16:54:36 -0800

    mongolab addon

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -5,7 +5,9 @@ var fs = require('fs');
 // require path for getting the models path
 var path = require('path');
 // connect to mongoose!
-mongoose.connect('mongodb://localhost/doctorapp');
+// mongoose.connect('mongodb://localhost/doctorapp');
+
+mongoose.connect('mongodb://tchou3:Guitarhero2!@ds011278.mongolab.com:11278/drapp');
 // create a variable that points to the path where all of the models live
 var models_path = path.join(__dirname, './../models');
 // read all of the files in the models_path and require (run) each of the javascript files
