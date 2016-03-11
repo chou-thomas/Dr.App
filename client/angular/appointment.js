910ceaba841abeb83b4544d7b900562b76544eb2
chou.thomas <chou.thomas3@gmail.com>
2016-03-11 00:23:42 -0800

    cleaned up appointments.js

--- client/angular/appointment.js
+++ client/angular/appointment.js
@@ -1,15 +1,21 @@
+//Angular code for creating, adding, getting appointments
+//File is acquired into the index.html file
+//appointment.html view partials can also use the methods, and controllers from the appointment.js
+//This file uses callbacks and uses the routes file to connect to the backend code.
+
+//Start of the appointment Factory
+//passes 
 myApp.factory('appointmentFactory',function($http){
-	var stuff = [];
-
 	var factory = {};
-
 	factory.getAppointments = function (callback){
+		console.log(callback);
 		$http.get('/appointments').success(function(output){
 				callback(output);
 			})	
 		}
 
 	factory.addAppointment = function(info, callback) {
+		console.log(info);
 		$http.post('/addappointment', info).success(function(output){
 			if(output.msg){
 				alert(output.msg);
@@ -20,57 +26,52 @@ myApp.factory('appointmentFactory',function($http){
 	}
 
 	factory.delete_appointment = function(info, callback) {
-		console.log(info);
 		$http.post('/deleteappointment', {'id':info}).success(function(output){
 			callback(output);
 		})
 	}
-    // most important step: return the object so it can be used by the rest of our angular code
+// most important step: return the object so it can be used by the rest of our angular code
     return factory;
 });
 
 
-
+//Start of the appointment Controller, takes in data from the specific factory method as an object
 myApp.controller('appointmentController', function ($scope, appointmentFactory, userFactory, $location){
-    //  initialize an empty array so $scope.orders maintains a consistent data type
+
+//  initialize an empty array so $scope.appointments maintains a consistent data type
     $scope.appointments = [];
     $scope.users =[];
     $scope.current_user = userFactory.name;
     appointmentFactory.getAppointments(function (data){
         $scope.appointments = data;
-        console.log(data);
     })
 
 	$scope.addAppointment = function() {
 		$scope.new_appointment.date = new Date();
 		$scope.new_appointment.name = $scope.current_user;
-		appointmentFactory.addAppointment($scope.new_appointment, function(){//callback
+		appointmentFactory.addAppointment($scope.new_appointment, function(){
 			appointmentFactory.getAppointments(
 				//callback
 				function(data){
-				console.log(data);
 				if(data.msg){
 					$location.path('/new_appointments');
 				}
 				else{
-				$scope.appointments = data;
-				$scope.new_appointment = {};
-				$location.path('/appointments');
-			}
+					$scope.appointments = data;
+					$scope.new_appointment = {};
+					$location.path('/appointments');
+				}
 			});
 		})
 	}
 
 	$scope.deleteAppointment = function(id) {
-		console.log(id);
 		appointmentFactory.delete_appointment(id, function(data){
 		appointmentFactory.getAppointments(function (data){
 		$scope.appointments = data;
 			})
-			console.log(data);
 			$scope.appointments = data;
 		})
 	}
     
-
 })
